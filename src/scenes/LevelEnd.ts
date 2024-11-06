f336f46718171c3664b37d396e6a079d90779891
Hope Fourie <hopefourie@gmail.com>
2024-11-06 14:11:32 -0500

    add level end copy

--- src/scenes/LevelEnd.ts
+++ src/scenes/LevelEnd.ts
@@ -10,7 +10,15 @@ export default class LevelEnd extends Phaser.Scene {
   create() {
     this.add.image(400, 300, "score");
     this.score = new Score(this, 100, 200);
-
+    const winText = this.add.text(
+      100,
+      100,
+      `Congrats! You reached your first \ntentative agreement!`,
+      {
+        stroke: '#000000',
+        strokeThickness: 4,
+        fontSize: "20px"}
+    ).setOrigin(0);
     const nextLevelButton = this.add.image(100, 250, "next").setScale(1 / 3).setOrigin(0);
     nextLevelButton.setInteractive({ useHandCursor: true });
     nextLevelButton.on("pointerup", () => {
